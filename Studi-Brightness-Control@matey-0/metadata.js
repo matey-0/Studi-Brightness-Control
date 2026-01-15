3028702732da6c52a8734dd3e7a6e2ce893f6723
Mateo <167565464+matey-0@users.noreply.github.com>
2026-01-15 17:50:58 -0500

    Update metadata.js

--- Studi-Brightness-Control@matey-0/metadata.js
+++ Studi-Brightness-Control@matey-0/metadata.js
@@ -2,7 +2,7 @@
   "name": "Studi Brightness Control",
   "description": "Native GNOME integration for Apple Studio Display via studi CLI",
   "uuid": "Studi-Brightness-Control@matey-0",
-  "shell-version": [ "45", "46", "47", "48", "49" ],
+  "shell-version": [ "48", "49" ],
   "url": "https://github.com/matey-0/Studi-Brightness-Control/",
   "version": 1
 }
