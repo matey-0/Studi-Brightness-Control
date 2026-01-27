7d4cef9047b88e4f8c8c37c5160321c1cab845d6
matey-0 <167565464+matey-0@users.noreply.github.com>
2026-01-27 12:03:30 -0500

    update, fix logic

--- Studi-Brightness-Control@matey-0/extension.js
+++ Studi-Brightness-Control@matey-0/extension.js
@@ -8,65 +8,35 @@ import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 
 const quickSettings = Main.panel.statusArea.quickSettings;
 
-function getDisplayCommand() {
-    let cmd = GLib.find_program_in_path('studi');
-    if (cmd) return cmd;
+async function execCommand(argv) {
+    try {
+        const proc = new Gio.Subprocess({
+            argv,
+            flags: Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE,
+        });
+        proc.init(null);
 
-    cmd = GLib.find_program_in_path('asdbctl');
-    if (cmd) return cmd;
+        const [stdout, stderr] = await proc.communicate_utf8(null, null);
+        if (!proc.get_successful())
+            throw new Error(stderr || 'Command failed');
 
-    const home = GLib.get_home_dir();
-    const studiCargo = `${home}/.cargo/bin/studi`;
-    if (GLib.file_test(studiCargo, GLib.FileTest.IS_EXECUTABLE)) {
-        return studiCargo;
+        return (stdout || '').trim().replace('brightness', '').trim();
+    } catch (e) {
+        throw e;
     }
-
-    const asdbCargo = `${home}/.cargo/bin/asdbctl`;
-    if (GLib.file_test(asdbCargo, GLib.FileTest.IS_EXECUTABLE)) {
-        return asdbCargo;
-    }
-
-    return studiCargo;
-}
-
-function execCommand(argv) {
-    return new Promise((resolve, reject) => {
-        try {
-            let proc = Gio.Subprocess.new(
-                argv,
-                Gio.SubprocessFlags.STDOUT_PIPE | Gio.SubprocessFlags.STDERR_PIPE
-            );
-
-            proc.communicate_utf8_async(null, null, (proc, res) => {
-                try {
-                    let [, stdout, stderr] = proc.communicate_utf8_finish(res);
-                    if (proc.get_successful()) {
-                        let output = stdout ? stdout.trim() : '';
-                        output = output.replace('brightness', '').trim();
-                        resolve(output);
-                    } else {
-                        reject(new Error(stderr || 'Command failed'));
-                    }
-                } catch (e) {
-                    reject(e);
-                }
-            });
-        } catch (e) {
-            reject(e);
-        }
-    });
 }
 
 const StudiSlider = GObject.registerClass(
 class StudiSlider extends QuickSettings.QuickSlider {
     _init(extension) {
-        const DISPLAY_CMD = getDisplayCommand();
-        const extensionDir = extension.dir;
-        const iconFile = Gio.File.new_for_path(extensionDir.get_path() + '/apple.svg');
-        const customIcon = Gio.icon_new_for_string(iconFile.get_uri());
+        this._extension = extension;
+        this._displayCmd = this._getDisplayCommand();
         
+        const iconFile = Gio.File.new_for_path(`${this._extension.path}/apple.svg`);
+        const gicon = Gio.Icon.new_for_string(iconFile.get_uri());
+
         super._init({
-            gicon: customIcon,
+            gicon,
         });
 
         this._isInternalUpdate = false;
@@ -74,52 +44,67 @@ class StudiSlider extends QuickSettings.QuickSlider {
         this._pendingValue = -1;
 
         this.slider.accessible_name = 'Studio Display Brightness';
-
-        this.slider.connect('notify::value', () => {
-            if (this._isInternalUpdate) return;
-
-            const percentage = Math.round(this.slider.value * 100);
-
-            if (this._updateTimeoutId) {
-                this._pendingValue = percentage;
-                return;
-            }
-
-            this._setBrightness(percentage);
-
-            this._updateTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
-                this._updateTimeoutId = 0;
-                if (this._pendingValue !== -1) {
-                    this._setBrightness(this._pendingValue);
-                    this._pendingValue = -1;
-                }
-                return GLib.SOURCE_REMOVE;
-            });
-        });
+        this.slider.connect('notify::value', () => this._onValueChanged());
 
         this.sync();
     }
 
-    destroy() {
-        if (this._updateTimeoutId) {
-            GLib.source_remove(this._updateTimeoutId);
-            this._updateTimeoutId = 0;
+    _getDisplayCommand() {
+        const binaries = ['studi', 'asdbctl'];
+        for (const bin of binaries) {
+            const path = GLib.find_program_in_path(bin);
+            if (path) return path;
         }
-        super.destroy();
+
+        const home = GLib.get_home_dir();
+        const cargoPaths = [
+            `${home}/.cargo/bin/studi`,
+            `${home}/.cargo/bin/asdbctl`,
+        ];
+
+        for (const path of cargoPaths) {
+            if (GLib.file_test(path, GLib.FileTest.IS_EXECUTABLE))
+                return path;
+        }
+
+        return 'studi';
+    }
+
+    _onValueChanged() {
+        if (this._isInternalUpdate) return;
+
+        const percentage = Math.round(this.slider.value * 100);
+
+        if (this._updateTimeoutId) {
+            this._pendingValue = percentage;
+            return;
+        }
+
+        this._setBrightness(percentage);
+
+        this._updateTimeoutId = GLib.timeout_add(GLib.PRIORITY_DEFAULT, 100, () => {
+            this._updateTimeoutId = 0;
+            if (this._pendingValue !== -1) {
+                this._setBrightness(this._pendingValue);
+                this._pendingValue = -1;
+            }
+            return GLib.SOURCE_REMOVE;
+        });
     }
 
     async _setBrightness(level) {
         try {
-            level = Math.max(0, Math.min(100, level));
-            await execCommand([DISPLAY_CMD, 'set', level.toString()]);
+            level = Math.clamp(level, 0, 100);
+            await execCommand([this._displayCmd, 'set', level.toString()]);
         } catch (e) {
+            console.error(e);
         }
     }
 
     async sync() {
         try {
-            const output = await execCommand([DISPLAY_CMD, 'get']);
-            let level = parseInt(output.trim());
+            const output = await execCommand([this._displayCmd, 'get']);
+            const level = parseInt(output);
 
             if (!isNaN(level)) {
                 this._isInternalUpdate = true;
@@ -132,8 +117,15 @@ class StudiSlider extends QuickSettings.QuickSlider {
         }
         return false;
     }
-});
 
+    destroy() {
+        if (this._updateTimeoutId) {
+            GLib.source_remove(this._updateTimeoutId);
+            this._updateTimeoutId = 0;
+        }
+        super.destroy();
+    }
+});
 
 const StudiIndicator = GObject.registerClass(
 class StudiIndicator extends QuickSettings.SystemIndicator {
@@ -144,30 +136,33 @@ class StudiIndicator extends QuickSettings.SystemIndicator {
         this.quickSettingsItems.push(this.slider);
         this.visible = false;
 
+        this._injectToGrid();
+    }
+
+    _injectToGrid() {
         const brightnessIndicator = quickSettings._brightness;
+        if (!brightnessIndicator) {
+            quickSettings.addExternalIndicator(this);
+            return;
+        }
+
         const brightnessSlider = brightnessIndicator.quickSettingsItems[0];
         const items = quickSettings.menu._grid.get_children();
         const brightnessIndex = items.indexOf(brightnessSlider);
         const nextItem = brightnessIndex >= 0 ? items[brightnessIndex + 1] : null;
 
-        if (brightnessSlider && nextItem) {
-            quickSettings.menu.insertItemBefore(
-                this.slider,
-                nextItem,
-                2
-            );
+        if (nextItem) {
+            quickSettings.menu.insertItemBefore(this.slider, nextItem, 2);
         } else {
-            quickSettings.addExternalIndicator(this, 2);
+            quickSettings.addExternalIndicator(this);
         }
     }
 
     destroy() {
         this.quickSettingsItems.forEach(item => item.destroy());
-        this.quickSettingsItems = [];
         super.destroy();
     }
 });
-
 
 export default class StudiExtension extends Extension {
     enable() {
@@ -177,9 +172,7 @@ export default class StudiExtension extends Extension {
         this._checkDisplay();
 
         this._menuSignal = quickSettings.menu.connect('open-state-changed', (menu, open) => {
-            if (open) {
-                this._checkDisplay();
-            }
+            if (open) this._checkDisplay();
         });
     }
 
@@ -187,7 +180,6 @@ export default class StudiExtension extends Extension {
         if (!this._slider || !this._indicator) return;
 
         const isConnected = await this._slider.sync();
-
         this._slider.visible = isConnected;
         this._indicator.visible = isConnected;
     }
@@ -202,5 +194,7 @@ export default class StudiExtension extends Extension {
             this._indicator.destroy();
             this._indicator = null;
         }
+        this._slider = null;
     }
 }
+
